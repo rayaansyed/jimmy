024efb353bd287c4ce32040f2b58888f2afcdbc0
rayaansyed <132567328+rayaansyed@users.noreply.github.com>
2023-05-09 03:50:02 -0700

    Update smr.java

--- smr.java
+++ smr.java
@@ -2,9 +2,9 @@
 public class smr{
   public static void main(String args[])
   {
-      int l = 40;
-      int b = 20;
-      ans = l*b;
+      int l=40;
+      int b=20;
+     int  ans =l*b;
       System.out.println("the area of rectangle is"+ ans);
   }
 }  
